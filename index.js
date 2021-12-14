061211591eea8b16269b86d71705057244f0e357
Amer Mall <amer@icloud.com>
2021-12-13 23:02:14 -0500

    commit tree 1837809031c312845560ae64259051ec307fbf90

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+// To start, define a employee variable and assign it to an Object containing name and streetAddress keys; you can use whatever values you like.
+
+const employee = {
+    name: "Bob",
+    streetAddress: "123 Street"
+};
+
+// 1. updateEmployeeWithKeyAndValue(): this function should take in three arguments: a employee Object, a key and a value. This function should not mutate the employee; it should return a new Object that has an updated value for the key passed in. Hint: use the spread operator!
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+
+    const newObj = { ...obj };
+    
+    newObj[key] = value;
+
+    return newObj;
+}
+
+// 2. destructivelyUpdateEmployeeWithKeyAndValue(): this function should work the same as updateEmployeeWithKeyAndValue() but it should mutate the employee Object passed in.
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+
+    return obj;
+}
+
+// 3. deleteFromEmployeeByKey(): this function should take in a employee Object and a key. It should delete the property with that key from the employee Object. This should not mutate the original employee Object; it should return a new Object that doesn't include the identified key-value pair. Hint: use the spread operator!
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = { ...obj };
+    delete newObj[key];
+    return newObj;
+}
+
+// 4. destructivelyDeleteFromEmployeeByKey(): this function should work the same as deleteFromEmployeeByKey() but it should mutate the employee Object.
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
